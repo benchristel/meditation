3de78f65a8357840906d758843dc6378da39e165
Ben Christel <ben.christel@gmail.com>
2024-03-19 18:16:35 -0700

    Add looping fountain audio

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,23 @@
 import {h} from "preact"
 import "./app.css"
+import {Gapless5} from "@regosen/gapless-5"
+
+// @ts-expect-error - cannot find module fountain.ogg
+import fountainAudioUrl from "./fountain.ogg"
+
+const backgroundNoisePlayer = new Gapless5({
+  tracks: [fountainAudioUrl],
+  loop: true,
+})
 
 export function App() {
   return (
     <div class="centering-frame">
       <div style={{width: 400, textAlign: "center", padding: "16px"}}>
         <div>
-          <button>Begin</button>
+          <button onClick={() => backgroundNoisePlayer.play()}>
+            Begin
+          </button>
         </div>
       </div>
     </div>
